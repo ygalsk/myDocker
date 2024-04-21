2213c3e08f7562e050af6677d6ab00e0015e6d2a
Daniel Kremer <dkremer@3-E-3.42heilbronn.de>
2024-04-21 17:54:25 +0200

    aded custom colors and msgs to see whats beign executed by the shell scrip

--- start.sh
+++ start.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Custom colors
+blue=$'\033[0;34m'
+green=$'\033[0;32m'
+red=$'\033[0;31m'
+reset=$'\033[0;39m'
+
 # Name of the Docker image
 IMAGE_NAME="dev-env"
 
@@ -9,8 +15,11 @@ CONTAINER_NAME="dev-container"
 # Path to the directory containing the Dockerfile
 DOCKERFILE_DIR="$(dirname "$0")"
 
-# Build the Docker image
-docker build -t "$IMAGE_NAME" "$DOCKERFILE_DIR"
+echo "${blue}Building Docker image...${reset}"
+# Build the Docker image with increased verbosity
+docker build --progress=plain -t "$IMAGE_NAME" "$DOCKERFILE_DIR" && \
+echo "${green}Docker image built successfully.${reset}"
 
+echo "${blue}Starting Docker container...${reset}"
 # Run the Docker container
 docker run -it --rm --name "$CONTAINER_NAME" "$IMAGE_NAME"
